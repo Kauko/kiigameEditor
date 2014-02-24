f05dac7917261a3ca42cb796a939fa30fd31483f
Mikko Nevala <mikkgo@gmail.com>
2014-02-24 16:45:09 +0200

    Started loading game data

--- View.py
+++ View.py
@@ -1,10 +1,14 @@
 import Object
+from random import randint
 
 # Virtual class for views
 class View(object):
-	def __init__(self):
-		self.id = ""
-		self.name = ""
+	def __init__(self, name, id=None):
+		# TODO: Check id collision, "running" id instead randint?
+		#		Static ID counter?
+		if not (id):
+			self.id = int(randint(0, 1000000000))
+		self.name = name
 		self.music = ""
 		self.type = ""
 
@@ -16,12 +20,18 @@ class Sequence(View):
 
 # Start menu
 class Menu(View):
-	def __init__(self):
-		super(Menu, self).__init__()
+	def __init__(self, name, id=None):
+		super(Menu, self).__init__(name, id)
 		self.background = ""
 		self.startButton = None
 		self.creditsButton = None
 		self.startImage = ""
+		
+		#self.addOption()
+		#self.addOption()
+		
+	def addOption(self):
+		print ("opstiooni")
 
 # Any game room
 class Room(View):
@@ -29,4 +39,4 @@ class Room(View):
 		super(Room, self).__init__()
 		self.objectList = []
 		self.background = ""
-		self.comingFrom = 
+		self.comingFrom = ""
